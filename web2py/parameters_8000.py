9ba278c8955a0ed96957c9b70f7ee85d86391ad4
victorsh91 <victor.sh.91@gmail.com>
2016-11-08 20:42:44 -0800

    fixed update
    fixed visual style on update.

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$82dff7f045808f4f$a7c84e35f6e9692758c118af5b3032e9ba8b1432"
+password="pbkdf2(1000,20,sha512)$9deb4f1bdbed9f2c$e185c7ebb480949a65baa4dc66b9dba948f7490f"
