70c82cd42309f488d2c88c01c0b3a8727ec8a4bf
SalvorMallow <brianjones86@gmail.com>
2016-11-10 17:03:47 -0800

    Tried to fix db

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9deb4f1bdbed9f2c$e185c7ebb480949a65baa4dc66b9dba948f7490f"
+password="pbkdf2(1000,20,sha512)$bc61c1614af990da$fc8d07c2ae79d84a628508d4aea05ced7ff1e490"
